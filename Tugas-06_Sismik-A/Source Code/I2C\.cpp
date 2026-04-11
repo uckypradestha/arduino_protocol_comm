a388bf36a7f46a3c76b0b954e8ee15a5135119c3
Ardhis Alivio Rajendra <alivvio56@gmail.com>
2026-04-11 21:31:39 +0700

    Add files via upload

---
+++ Tugas-06_Sismik-A/Source Code/I2C\.cpp
@@ -0,0 +1,40 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2); 
+
+String barisAtas = "QUOTE"; 
+
+String pesan = "HAVE A NICE DAY -- ARDHIS ALIVIO -- 31"; 
+String fullPesan = "                " + pesan + "                "; 
+
+unsigned long waktuLalu = 0;
+int pos = 0;
+const int kecepatan = 120; 
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  
+  lcd.setCursor(5, 0); 
+  lcd.print(barisAtas);
+}
+
+void loop() {
+  unsigned long waktuSekarang = millis();
+
+  if (waktuSekarang - waktuLalu >= kecepatan) {
+    waktuLalu = waktuSekarang;
+
+   
+    String sub = fullPesan.substring(pos, pos + 16);
+    
+    lcd.setCursor(0, 1);
+    lcd.print(sub); 
+    pos++;
+    
+    // Reset posisi 
+    if (pos >= fullPesan.length() - 16) {
+      pos = 0;
+    }
+  }
+}
