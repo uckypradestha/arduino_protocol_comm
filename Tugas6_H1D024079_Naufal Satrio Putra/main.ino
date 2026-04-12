ec9b91c86c5dffdd36678051aef47db9e0acf315
naufal <naufalsatrioputra@gmail.com>
2026-04-12 21:29:36 +0700

    Add: Tugas 4 Pedestrian Traffic Light - Naufal Satrio Putra

---
+++ Tugas6_H1D024079_Naufal Satrio Putra/main.ino
@@ -0,0 +1,18 @@
+#include <Adafruit_LiquidCrystal.h>
+
+Adafruit_LiquidCrystal lcd(0);
+
+void setup() {
+  lcd.begin(16, 4);
+  
+  // Menyalakan lampu latar
+  lcd.setBacklight(1);
+  lcd.print("Selamat menikmati MBG");
+  delay(1000);
+}
+
+void loop() {
+  // Jalan ke kiri bertahap
+  lcd.scrollDisplayLeft();
+  delay(300);
+}
